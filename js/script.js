b40a48c162fdc2cff515825fa7983cd91e2f32f4
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-01 11:15:58 -0300

    up

--- js/script.js
+++ js/script.js
@@ -3,28 +3,29 @@ const button = document.getElementById("toAdd");
 const getAllList = document.getElementsByTagName("li");
 
 button.addEventListener("click", function(){
+
     var getInput = document.getElementById("inputBook").value;
 
-        if(getAllList.length > 0){
+   if ( getInput.length >= 1 ){
 
-            for(var i = 0; i < getAllList.length; 1){
+   	 for(var i = 0; i < getAllList.length; i){
                 getAllList[i].textContent = getInput;
-            }
-        }
-        
+            } 
+   } 
+
+})  
+       
+        /*
+ var list = getAllList[0];
+
+     list.textContent = getInput;   
+        */
 
         
        
-})  
+
 
 /*
-var getInput = document.getElementById("inputBook").value;
-
-var list = getAllList[0];
-
-list.textContent = getInput;
-
-list = getAllList[1];
-
-list.textContent = getInput;
+if(getAllList.length > 0){
+           
 */
