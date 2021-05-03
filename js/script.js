8a286f94e89be7e2d2ac05834094fef74557f5a9
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-02 23:43:59 -0300

    add done, line down done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 // Pega o button.
 const button = document.getElementById("toAdd");
 
@@ -14,67 +6,16 @@ let getInput = document.getElementById("inputBook");
 
 let ul = document.getElementById("list-ul");
 
-let li = document.createElement("li");
- li.className = 'list';
 
 
 // Ativa o button no click
 button.addEventListener("click", function(){
 
+ let li = document.createElement("li");
+  li.className = 'list';
 
   li.setAttribute('id', getInput.value);
   li.appendChild(document.createTextNode(getInput.value));
   ul.appendChild(li);
   
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const button = document.getElementById("toAdd");
-
-let getAllList = document.getElementsByTagName("li")[0];
-
-if(getAllList > 1){
-	getAllList ++;
-}
-
-
-let getInput = document.getElementById("inputBook").value;
-
-
-button.addEventListener("click", function(){
-
-
-   getAllList.textContent = getInput;
-
-    
-
-})  
-*/
-       
-  
-
-
-
-
-
-
-/*
- if ( getInput.length >= 1 ){
-
-   	 for(var i = 0; i < getAllList.length; i){
-                getAllList[i].textContent = getInput;
-            } 
-   } 
-*/
