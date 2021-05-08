2250af26890114e3b9c93ef55da9e5800a1c8051
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-08 15:56:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,7 @@ const button = document.getElementById("toAdd");
 let getInput = document.getElementById("inputBook");
 
 let ul = document.getElementById("list-ul");
-/*
-function x(){
-	for(index = 0; element.length > 0; index++){
-  			let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
 
-      containerQuantityBooks.textContent =  "Você já leu " + 10 + " livros";
-  }
-}
-*/
 
 // Ativa o button no click
 button.addEventListener("click", function(){
@@ -25,60 +17,24 @@ button.addEventListener("click", function(){
   ul.appendChild(li);
 
   let element = document.getElementsByTagName("li");
-
-
-
-  if(element){
-
-      let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
-
-      for(element = 1; element > 0; element++){
-      	containerQuantityBooks.textContent =  "Você já leu " + element + " livros";
-      }
-
-      
-  }
-})
-  /*
-  function countItems(arr) {
-  const countMap = Object.create(null);
-
-  for (const element of arr) {
-    if (!countMap[element]) {
-      // Se ainda não existir elemento, definimos como um, já que
-      // estamos na primeira ocorrência.
-      countMap[element] = 1;
-    } else {
-      // Caso contrário, incrementamos um no número atual.
-      countMap[element] += 1;
-    }
-  }
   
-  return countMap;
-}
+  for(var i = 5; i > 0; i++){
+  	
+    
+     let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
+   
+      containerQuantityBooks.textContent =  "Você já leu " + i + " livros";             
+ 
+  }
+ 
 
-const arr = ['1', '1', '2', '2', '2'];
-console.log(countItems(arr));
-let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
+})
 
-      containerQuantityBooks.textContent =  "Você já leu " + (countItems(arr)) + " livros";
-      
-
-      /*
-
-      for(const a of arr){
-  		if(!element){
-  			let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
-
-      containerQuantityBooks.textContent =  "Você já leu " + 10 + " livros";
-  		}
-  	}else {
-  		let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
-
-      containerQuantityBooks.textContent =  "Você já leu " + 77 + " livros";
-  	}
-
-  	const arr = ['1', '1', '2', '2', '2'];
-
-  	 return li;
-  	 */
+/*
+ if(element){
+    
+     let containerQuantityBooks = document.getElementById("container-quantityBooks"); 
+   
+      containerQuantityBooks.textContent =  "Você já leu " + i + " livros";             
+  }
+*/
