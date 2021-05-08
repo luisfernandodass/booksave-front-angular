29932226bffa291a0e40c2017b960bdcd32d6444
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-08 18:06:46 -0300

    i putted actions after button click on js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ function adicionar(e){
   let ul = document.getElementById("list-ul"); // Get the biggest parentNode: ul
     
   let li = document.createElement("li");
-  li.className = 'list';
 
+  li.className = 'list';
+  li.innerHTML = '<i class="fas fa-eye eye"></i> <i class="fas fa-pen-square pen"></i><i class="fas fa-minus-circle delete"></i>'
+  
   li.setAttribute('id', getInput.value);
   li.appendChild(document.createTextNode(getInput.value));
+  
   ul.appendChild(li);
+ 
 }
  
 const add = document.getElementById("toAdd");
