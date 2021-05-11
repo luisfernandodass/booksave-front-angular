2c07a6aa54e80786e99a865e0ab29e50d3193d4b
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-11 09:18:47 -0300

    delete fooooooooooooooooooiiii

--- js/script.js
+++ js/script.js
@@ -7,12 +7,28 @@ add.addEventListener("click", function(e){
   let li = document.createElement("li");
  
   li.className = 'actions';
+
+  li.id = 'aaa';
   li.innerHTML = '<i class="fas fa-eye eye"></i> '
   li.innerHTML += '<i class="fas fa-pen-square pen"></i>'
-  li.innerHTML += '<i id="ex" class="fas fa-minus-circle delete"></i>'
+  li.innerHTML += '<i class="fas fa-minus-circle delete" onclick="exclude()"></i>'
   
  
   li.appendChild(document.createTextNode(getInput.value));
 
   ul.appendChild(li);
+
+
+  
 })
+
+function exclude(e){
+  
+  var li = document.getElementById('aaa');
+
+   let ul = document.getElementById("list-ul");
+
+  if(ul){
+    ul.removeChild(li);
+  }
+}
