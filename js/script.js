613b56d3ef32ad223ef9d64c49c2afceb18a3f4b
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-10 22:47:28 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,34 +1,18 @@
-function adicionar(e){
-  let getInput = document.getElementById("inputBook"); // Get the input book name.
+const add = document.getElementById("toAdd");
+add.addEventListener("click", function(e){
+   let getInput = document.getElementById("inputBook"); // Get the input book name.
 
   let ul = document.getElementById("list-ul"); // Get the biggest parentNode: ul
     
   let li = document.createElement("li");
-
- 
  
   li.className = 'actions';
   li.innerHTML = '<i class="fas fa-eye eye"></i> '
   li.innerHTML += '<i class="fas fa-pen-square pen"></i>'
-  li.innerHTML += '<i class="fas fa-minus-circle delete"></i>'
+  li.innerHTML += '<i id="ex" class="fas fa-minus-circle delete"></i>'
   
+ 
   li.appendChild(document.createTextNode(getInput.value));
- 
+
   ul.appendChild(li);
-  
-}
- 
-const add = document.getElementById("toAdd");
-add.addEventListener("click", function(e){
-    adicionar(e);
 })
-
-function excluir(e){
-   
-}
-
-
-const exclude = document.getElementByClassName('delete');
-exclude.addEventListener("click", function(e){ 
-        excluir(e);
-})   
