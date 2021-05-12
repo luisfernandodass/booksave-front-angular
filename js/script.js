ee5a5f5640ababe704390b1e1c49760b2287a3ed
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-12 10:29:32 -0300

    separei os actions do namebook

--- js/script.js
+++ js/script.js
@@ -7,13 +7,11 @@ add.addEventListener("click", function(e){
     
   let li = document.createElement("li");
  
-  li.className = 'actions';
+  
 
-  li.id += 'act';
-  li.innerHTML = '<p id="nameBook">' + getInput.value + '</p>';
-  li.innerHTML += '<i class="fas fa-eye eye" onclick="read()"></i> '
-  li.innerHTML += '<i class="fas fa-pen-square pen" onclick="update()"></i>'
-  li.innerHTML += '<i id="exclude" class="fas fa-minus-circle delete" onclick="exclude()"></i>'
+  li.id = 'act';
+  li.innerHTML = '<p id="nameBook" class="nameBook">' + getInput.value + '</p>';
+  li.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="read()"></i><i class="fas fa-pen-square pen" onclick="update()"></i><i id="exclude" class="fas fa-minus-circle delete" onclick="exclude()"></i></div> ';
   
   ul.appendChild(li);
 })
@@ -33,10 +31,21 @@ function exclude(e){
 
 function update(e){
 
+  /*
   let li = document.getElementById('nameBook');
 
   li.textContent = "fffff";
+  */
+
+  let li = document.getElementById('act');
+
+   let ul = document.getElementById("list-ul");
+
+  if(ul){
+    ul.replace(li, "kkkk");
+  }
 }
+
 
 function read(e){
   let li = document.getElementById('nameBook');
