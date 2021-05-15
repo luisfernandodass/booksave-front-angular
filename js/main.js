cbe143f3ba7475a1fd7e4c57eb4640a2f6661e3d
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-15 19:33:49 -0300

    funcionalidade de contar checked

--- js/main.js
+++ js/main.js
@@ -1,54 +1,35 @@
-const add = document.getElementById("toAdd");
-add.addEventListener("click", function(e){
+var containerOfBook = document.getElementById("containerOfBook");
+var buttonToAdd  = document.getElementById("toAdd");
 
+function addBook(e){
   let getInput = document.getElementById("inputBook"); // Get the input book name.
-container-booksReadsList
-  let ul = document.getElementById("list-ul"); // Get the biggest parentNode: ul
-    
-  let li = document.createElement("tr");
+  
+  let row = document.createElement("tr");
  
+  row.id = 'containerRow'; 
+  row.innerHTML = '<p id="nameBook" class="nameBook">' + getInput.value + '</p>';
+  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="read()"></i><i class="fas fa-pen-square pen" onclick="update()"></i><i class="fas fa-minus-circle delete" onclick="removeBook()"></i></div> ';
   
+  containerOfBook.appendChild(row);
+}
 
-  li.id = 'act';
-  
-  li.innerHTML = '<p id="nameBook" class="nameBook">' + getInput.value + '</p>';
-  li.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="read()"></i><i class="fas fa-pen-square pen" onclick="update()"></i><i id="exclude" class="fas fa-minus-circle delete" onclick="exclude()"></i></div> ';
-  
-  ul.appendChild(li);
-})
-
-
-function exclude(e){
-  
-   let li = document.getElementById('act');
-
-   let ul = document.getElementById("list-ul");
-
-  if(ul){
-    ul.removeChild(li);
+function counterBook(){
+  let counter = document.getElementById("counter");
+  let qntdBook = document.getElementsByTagName('tr').length;
+  if (qntdBook === 1){
+    counter.textContent =  qntdBook + " livro lido";
+  } else {
+    counter.textContent =  qntdBook + " livros lidos";
   }
 }
 
+function removeBook(e){
+   let row = document.getElementById('containerRow');  
 
-function update(e){
-
-  /*
-  let li = document.getElementById('nameBook');
-
-  li.textContent = "fffff";
-  */
-
-  let li = document.getElementById('act');
-
-   let ul = document.getElementById("list-ul");
-
-  if(ul){
-    ul.replace(li, "kkkk");
+  if(containerOfBook){
+    containerOfBook.removeChild(row);
   }
 }
 
-
-function read(e){
-  let li = document.getElementById('nameBook');
-  alert(li.textContent);
-}
+buttonToAdd.addEventListener('click', addBook, false);
+buttonToAdd.addEventListener('click', counterBook, false);
