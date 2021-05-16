8985fed1bc8851da5d710de2c412bf814d242989
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-16 11:33:12 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var containerOfBook = document.getElementById("containerOfBook");
 var buttonToAdd  = document.getElementById("toAdd");
- var counter = document.getElementById("counter");
+var counter = document.getElementById("counter");
+
 function addBook(e){
   let getInput = document.getElementById("inputBook"); // Get the input book name.
   
@@ -8,19 +9,24 @@ function addBook(e){
  
   row.id = 'containerRow'; 
   row.innerHTML = '<p id="nameBook" class="nameBook">' + getInput.value + '</p>';
-  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="read()"></i><i class="fas fa-pen-square pen" onclick="update()"></i><i class="fas fa-minus-circle delete" onclick="removeBook()"></i></div> ';
+  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye"></i><i id="update" class="fas fa-pen-square pen"></i><i class="fas fa-minus-circle delete" onclick="removeBook()"></i></div> ';
   
   containerOfBook.appendChild(row);
 }
 
 function counterBook(){
  
-  var qntdBook = document.getElementsByTagName('tr').length;
+  let qntdBook = document.getElementsByTagName('tr').length;
   if (qntdBook === 1){
     counter.textContent =  qntdBook + " livro lido";
   } else {
     counter.textContent =  qntdBook + " livros lidos";
   }
+}
+
+function updateBook(){
+  var update  = document.getElementById("update");
+  prompt("opaaaaaa");
 }
 
 function removeBook(e){
@@ -29,10 +35,14 @@ function removeBook(e){
   if(containerOfBook){
     containerOfBook.removeChild(row);
    
-    var qntdBook = document.getElementsByTagName('tr').length--;
+    let qntdBook = document.getElementsByTagName('tr').length--;
     counter.textContent =  qntdBook + " livros lidos";
+    if(qntdBook === 0){
+      counter.textContent =  "";
+    }
   }
 }
 
 buttonToAdd.addEventListener('click', addBook, false);
 buttonToAdd.addEventListener('click', counterBook, false);
+update.addEventListener('click', updateBook, false);
