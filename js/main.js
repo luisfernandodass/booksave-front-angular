e73a52c07846eb89a77a330d82993231a3b35c89
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-21 20:39:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var container = document.getElementById("container-booksReadsList"); // Containe
 var buttonToAdd  = document.getElementById("toAdd"); // Botão de adicionar
 var counter = document.getElementById("counter");  // Conta a quantidade de livros
 
-
 function addBook(e){
   container.className = "container-booksReadsList"
   let getInput = document.getElementById("inputBook"); // Pega o nome do livro no input.
@@ -13,11 +12,10 @@ function addBook(e){
   row.setAttribute("id", getInput.value);    // Atualizar o valor do ID para o nome do livro.
   row.className += 'containerRow'; 
   row.innerHTML += '<p class="nameBook">' + getInput.value + '</p>';
-  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye"></i><i class="fas fa-pen-square pen" ></i><i class="fas fa-minus-circle delete" onclick="removeBook(event)"></i></div> ';
+  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="see(event)"></i><i class="fas fa-pen-square pen" ></i><i class="fas fa-minus-circle delete" onclick="removeBook(event)"></i></div> ';
   
   containerOfBook.appendChild(row); // Adiciona o livro na tabela
  
-  
    // Abaixo a parte de contagem dos livros
   let qntdBook = document.getElementsByTagName('tr').length; // Incrementa a quantidade de livros, conforme o user for adicionando.
   if (qntdBook === 1){
@@ -27,13 +25,24 @@ function addBook(e){
   }
 }
 
-function btnRemove(event){
+function nameBook(event){
   return event.target;
 }
 
+// Abaixo acesso o elemento navegando pelo DOM
+function see(event){
+  var deleteButton, parentButton, grandParentButton, greatGrandFather;
+  seeButton = nameBook(event);                        // Botão delete
+  parentButton = seeButton.parentNode;                // Pega o pai do botão delete (actions)
+  grandParentButton = parentButton.parentNode;        // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
+  greatGrandFather = grandParentButton.firstChild.textContent; // Pega o texto da tag <p> (nome do livro)
+  alert(greatGrandFather);
+}
+
+// Abaixo acesso o elemento navegando pelo DOM
 function removeBook(event){
   var deleteButton, parentButton, grandParentButton, greatGrandFather;
-  deleteButton = btnRemove(event);                       // Botão delete
+  deleteButton = nameBook(event);                       // Botão delete
   parentButton = deleteButton.parentNode;                // Pega o pai do botão delete (actions)
   grandParentButton = parentButton.parentNode;     // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
   greatGrandFather = grandParentButton.parentNode; // Pega o pai do containerRow (containerOfBook)
