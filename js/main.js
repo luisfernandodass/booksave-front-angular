37e740af36544611121bff39db17c6a5d47f1a44
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-31 13:26:42 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-
-var containerOfBook = document.getElementById("containerOfBook");  // Container branco
-var container = document.getElementById("container-booksReadsList"); 
-var buttonToAdd  = document.getElementById("toAdd"); // Botão de adicionar
+const containerOfBook = document.getElementById("containerOfBook");  // Container branco
+const container = document.getElementById("container-booksReadsList"); 
+const buttonToAdd  = document.getElementById("toAdd"); // Botão de adicionar
 var counter = document.getElementById("counter");  // Conta a quantidade de livros
-var actionButton, parentButton, grandParentButton, greatGrandFather; // Variaveis usadas nas functions see() & removeBook()
+var targetButton, parentButton, grandParentButton, greatGrandFather;
 
-// Pega o alvo (o elemento onde o livro está) e é usado nas functions see() & event()
-function nameBook(event){     
-  return event.target;
-}
-
-function increaseQuantityBooks(){
+function increaseQuantityOfBooks(){
   let qntdBook = document.getElementsByTagName('tr').length; // Incrementa a quantidade de livros, conforme o user for adicionando.
   if (qntdBook === 1){
     counter.textContent =  qntdBook + " livro lido";
@@ -19,16 +13,22 @@ function increaseQuantityBooks(){
   }
 }
 
-function decreaseQuantityBook(){
+function decreaseQuantityOfBooks(){
    let qntdBook = document.getElementsByTagName('tr').length--;  // Decrementa a quantidade de livros, conforme o user for excluindo.
     if(qntdBook === 0){
       counter.textContent =  ""; // Remove a contagem dos livros.
       container.className = ""   // Remove o container branco.
     } else if (qntdBook === 1) {
-       counter.textContent =  qntdBook + " livros lido";
+       counter.textContent =  qntdBook + " livro lido";
     } else {
        counter.textContent =  qntdBook + " livros lidos";
     }   
+}
+
+function getRowOfBook(event){
+    targetButton = event.target;         // Pega o elemento alvo (delete button or getBook button)
+    parentButton = targetButton.parentNode;       // Get the parent of target button (class actions)
+    grandParentButton = parentButton.parentNode;  // Get the parenf of the class actions (the tag 'tr' || a class 'containerRow') - a linha do livro
 }
 
 function addBook(e){
@@ -38,31 +38,25 @@ function addBook(e){
   let row = document.createElement("tr");    // Cria a linha do livro
   row.className += 'containerRow'; 
   row.innerHTML += '<p class="nameBook">' + getInput.value + '</p>';
-  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="see(event)"></i><i class="fas fa-pen-square pen" ></i><i class="fas fa-minus-circle delete" onclick="removeBook(event)"></i></div> ';
+  row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="getBook(event)"></i><i class="fas fa-pen-square pen" ></i><i class="fas fa-minus-circle delete" onclick="removeBook(event)"></i></div> ';
   
   containerOfBook.appendChild(row); // Adiciona o livro na tabela
  
-  increaseQuantityBooks();
+  increaseQuantityOfBooks();
 }
 
-function getLineOfBook(){
-    actionButton = nameBook(event);                                    // Botão delete
-    parentButton = actionButton.parentNode;                            // Pega o pai do botão delete (actions)
-    grandParentButton = parentButton.parentNode;                       // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
-}
-
-function see(event){
-  getLineOfBook();
+function getBook(event){
+  getRowOfBook(event);
   greatGrandFather = grandParentButton.firstChild.textContent;       // Pega o texto da tag <p> (nome do livro)
   alert(greatGrandFather);
 }
 
 function removeBook(event){ 
-  getLineOfBook();
+  getRowOfBook(event);
   greatGrandFather = grandParentButton.parentNode; // Pega o pai do containerRow (containerOfBook)
   greatGrandFather.removeChild(grandParentButton);
 
-  decreaseQuantityBook();
+  decreaseQuantityOfBooks();
 }
 
 buttonToAdd.addEventListener('click', addBook, false);
