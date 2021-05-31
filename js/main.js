7d7eed26f8e74a59021f32f74d5e6e8a218b484b
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-31 12:55:51 -0300

    refactored (more simple to understand now)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,33 @@ var containerOfBook = document.getElementById("containerOfBook");  // Container 
 var container = document.getElementById("container-booksReadsList"); 
 var buttonToAdd  = document.getElementById("toAdd"); // Botão de adicionar
 var counter = document.getElementById("counter");  // Conta a quantidade de livros
+var actionButton, parentButton, grandParentButton, greatGrandFather; // Variaveis usadas nas functions see() & removeBook()
+
+// Pega o alvo (o elemento onde o livro está) e é usado nas functions see() & event()
+function nameBook(event){     
+  return event.target;
+}
+
+function increaseQuantityBooks(){
+  let qntdBook = document.getElementsByTagName('tr').length; // Incrementa a quantidade de livros, conforme o user for adicionando.
+  if (qntdBook === 1){
+    counter.textContent =  qntdBook + " livro lido";
+  } else {
+    counter.textContent =  qntdBook + " livros lidos";
+  }
+}
+
+function decreaseQuantityBook(){
+   let qntdBook = document.getElementsByTagName('tr').length--;  // Decrementa a quantidade de livros, conforme o user for excluindo.
+    if(qntdBook === 0){
+      counter.textContent =  ""; // Remove a contagem dos livros.
+      container.className = ""   // Remove o container branco.
+    } else if (qntdBook === 1) {
+       counter.textContent =  qntdBook + " livros lido";
+    } else {
+       counter.textContent =  qntdBook + " livros lidos";
+    }   
+}
 
 function addBook(e){
   container.className = "container-booksReadsList" // Faz aparecer o container branco
@@ -15,48 +42,27 @@ function addBook(e){
   
   containerOfBook.appendChild(row); // Adiciona o livro na tabela
  
-   // Abaixo a parte de contagem dos livros
-  let qntdBook = document.getElementsByTagName('tr').length; // Incrementa a quantidade de livros, conforme o user for adicionando.
-  if (qntdBook === 1){
-    counter.textContent =  qntdBook + " livro lido";
-  } else {
-    counter.textContent =  qntdBook + " livros lidos";
-  }
+  increaseQuantityBooks();
 }
 
-function nameBook(event){     // Pega o alvo (o elemento onde o livro está) e é usado nas functions see() & event()
-  return event.target;
+function getLineOfBook(){
+    actionButton = nameBook(event);                                    // Botão delete
+    parentButton = actionButton.parentNode;                            // Pega o pai do botão delete (actions)
+    grandParentButton = parentButton.parentNode;                       // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
 }
 
- var actionButton, parentButton, grandParentButton, greatGrandFather; // Variaveis usadas nas functions see() & removeBook()
-
-// Abaixo acesso o elemento navegando pelo DOM
 function see(event){
-  actionButton = nameBook(event);                                    // Botão delete
-  parentButton = actionButton.parentNode;                            // Pega o pai do botão delete (actions)
-  grandParentButton = parentButton.parentNode;                       // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
+  getLineOfBook();
   greatGrandFather = grandParentButton.firstChild.textContent;       // Pega o texto da tag <p> (nome do livro)
   alert(greatGrandFather);
 }
 
-// Abaixo acesso o elemento navegando pelo DOM
 function removeBook(event){ 
-  actionButton = nameBook(event);                       // Botão delete
-  parentButton = actionButton.parentNode;                // Pega o pai do botão delete (actions)
-  grandParentButton = parentButton.parentNode;     // Pega o pai do actions (a tag 'tr' || a class 'containerRow') - a linha do livro
+  getLineOfBook();
   greatGrandFather = grandParentButton.parentNode; // Pega o pai do containerRow (containerOfBook)
   greatGrandFather.removeChild(grandParentButton);
 
-  // Abaixo a parte de contagem dos livros
-  let qntdBook = document.getElementsByTagName('tr').length--;  // Decrementa a quantidade de livros, conforme o user for excluindo.
-    if(qntdBook === 0){
-      counter.textContent =  ""; // Remove a contagem dos livros.
-      container.className = ""   // Remove o container branco.
-    } else if (qntdBook === 1) {
-       counter.textContent =  qntdBook + " livros lido";
-    } else {
-       counter.textContent =  qntdBook + " livros lidos";
-    }   
+  decreaseQuantityBook();
 }
 
 buttonToAdd.addEventListener('click', addBook, false);
