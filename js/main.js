83c3704bd0fcde94736b062317df9ffca3e5b36f
Luis Fernando <67171626+luiszf@users.noreply.github.com>
2021-05-31 23:42:59 -0300

    foi array no consolel, irraaa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const containerOfBook = document.getElementById("containerOfBook");  // Container branco
 const container = document.getElementById("container-booksReadsList"); 
-const buttonToAdd  = document.getElementById("toAdd"); // Botão de adicionar
 var counter = document.getElementById("counter");  // Conta a quantidade de livros
 var targetButton, parentButton, grandParentButton, greatGrandFather;
 
@@ -28,22 +27,28 @@ function decreaseQuantityOfBooks(){
 function getRowOfBook(event){
     targetButton = event.target;         // Pega o elemento alvo (delete button or getBook button)
     parentButton = targetButton.parentNode;       // Get the parent of target button (class actions)
-    grandParentButton = parentButton.parentNode;  // Get the parenf of the class actions (the tag 'tr' || a class 'containerRow') - a linha do livro
+    grandParentButton = parentButton.parentNode;  // Get the parent of the class actions (the tag 'tr' || a class 'containerRow') - a linha do livro
 }
 
-function addBook(e){
+function addBook(event){
   container.className = "container-booksReadsList" // Faz aparecer o container branco
   let getInput = document.getElementById("inputBook"); // Pega o nome do livro no input.
   
+  var b = [];
+
   let row = document.createElement("tr");    // Cria a linha do livro
   row.className += 'containerRow'; 
   row.innerHTML += '<p class="nameBook">' + getInput.value + '</p>';
   row.innerHTML += '<div class="actions"><i class="fas fa-eye eye" onclick="seeBook(event)"></i><i class="fas fa-pen-square pen" ></i><i class="fas fa-minus-circle delete" onclick="removeBook(event)"></i></div> ';
   
   containerOfBook.appendChild(row); // Adiciona o livro na tabela
- 
+
+  b.push(containerOfBook.appendChild(row).textContent);
+  console.log(b);
+
   increaseQuantityOfBooks();
 }
+
 
 function seeBook(event){
   getRowOfBook(event);
@@ -55,8 +60,6 @@ function removeBook(event){
   getRowOfBook(event);
   greatGrandFather = grandParentButton.parentNode; // Pega o pai do containerRow (containerOfBook)
   greatGrandFather.removeChild(grandParentButton);
-
+  localStorage.removeItem('book');
   decreaseQuantityOfBooks();
 }
-
-buttonToAdd.addEventListener('click', addBook, false);
