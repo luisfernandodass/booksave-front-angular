d9970cb54981bffd940d7536a904392bafbbba98
Luis Fernando <luisfernandodass@gmail.com>
2021-08-22 10:50:35 -0300

    js it's ok, just one mistake about child el

--- js/count-books.js
+++ js/count-books.js
@@ -1,21 +1,25 @@
-  let counter = document.querySelector(".counter"); // Conta a quantidade de livros
-  let howManyBooks = document.getElementsByTagName("tr").length;
+let counter = document.querySelector(".counter");
+let howManyBooks = document.getElementsByTagName("tr").length;
+const container = document.querySelector(".container-booksReadsList");
 
-  export function increaseQuantityOfBooks() {
-    howManyBooks++;
-    howManyBooks === 1
-      ? (counter.innerText = `${howManyBooks} livro lido`)
-      : (counter.innerText = `${howManyBooks} livros lidos`);
+export function increaseQuantityOfBooks() {
+  howManyBooks++;
+  if (howManyBooks === 1) {
+    counter.innerText = `${howManyBooks} livro lido`;
+    container.style.display = "block";
+  } else {
+    counter.innerText = `${howManyBooks} livros lidos`;
   }
+}
 
-  export function decreaseQuantityOfBooks(event) {
-    howManyBooks--;
-    if (howManyBooks === 0) {
-      counter.innerText = ""; // Remove a contagem dos livros.
-      container.classList.remove("container-booksReadsList"); // Remove o container branco.
-    } else if (howManyBooks === 1) {
-      counter.innerText = `${howManyBooks} livro lido`;
-    } else {
-      counter.innerText = `${howManyBooks} livros lidos`;
-    }
+export function decreaseQuantityOfBooks() {
+  howManyBooks--;
+  if (howManyBooks === 0) {
+    counter.innerText = "";
+    container.style.display = "none";
+  } else if (howManyBooks === 1) {
+    counter.innerText = `${howManyBooks} livro lido`;
+  } else {
+    counter.innerText = `${howManyBooks} livros lidos`;
   }
+}
