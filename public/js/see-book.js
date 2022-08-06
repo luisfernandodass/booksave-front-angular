dbbceb6e6579ef6a1acdc6cf1f297c1caaa4917d
Luis Fernando <luisfernandodass@gmail.com>
2022-08-06 15:45:22 -0300

    feat: wip - adicionando webpack

--- public/js/see-book.js
+++
@@ -1,8 +0,0 @@
-export default function seeBook() {
-  const btnSeeBook = document.querySelectorAll(".eye");
-  btnSeeBook.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      alert(e.target.parentNode.previousElementSibling.textContent);
-    });
-  });
-}
---
+++ src/js/delete-book.js
@@ -0,0 +1,14 @@
+import { counterDecrease } from "./counter-book.js";
+import { removeBookInStorage,} from "./localstorage.js"
+
+export function deleteBook() {
+    const table = document.querySelector(".table"); // Container branco
+    const btnDeleteBook = document.querySelectorAll(".delete");
+    btnDeleteBook.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        table.removeChild(e.target.parentNode.parentNode);
+        counterDecrease();
+        removeBookInStorage(e.target.parentNode.parentNode);
+      });
+    });
+  }
