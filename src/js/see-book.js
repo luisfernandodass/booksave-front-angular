75a0c35f068de8da3f25a9e05ffb134261f55ef6
Luis Fernando <luisfernandodass@gmail.com>
2022-08-20 19:16:13 -0300

    commit inicial - adicionado angular

--- src/js/see-book.js
+++
@@ -1,8 +0,0 @@
-export function seeBook() {
-  const btnSeeBook = document.querySelectorAll(".eye");
-  btnSeeBook.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      alert(e.target.parentNode.previousElementSibling.textContent);
-    });
-  });
-}
